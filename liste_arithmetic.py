35fd7eacea1d9ab15632b5478ebc8f2ecdc80ce1
julien genty <julien.genty80@gmail.com>
2023-01-23 00:29:22 +0100

    ajout de la fonction de verification d'une liste arithmetique

--- liste_arithmetic.py
+++ liste_arithmetic.py
@@ -1,2 +1,9 @@
 def isArithmetic(liste):
-    pass
+    if type(liste) == list and liste != []: 
+        raison = liste[1]-liste[0]
+        for i in range(1, len((liste))-1) :
+            if (liste[i+1]-liste[i]) != raison :
+                return False
+        return True
+    else :
+        return False
