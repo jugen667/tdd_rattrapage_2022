ae04eee5fdc20a8a7fa8300f8cd3a735e5cd5e90
julien genty <julien.genty80@gmail.com>
2023-01-23 00:15:15 +0100

    ajout de la fonction de verification d'une liste geometrique

--- liste_geometrique.py
+++ liste_geometrique.py
@@ -1,2 +1,15 @@
 def isGeometric(liste):
-    pass
+    if type(liste) == list and liste != []: 
+        if liste[0] != 0 :
+            raison = liste[1]/liste[0]
+            for i in range(1, len((liste))-1) :
+                if liste[i] != 0 :
+                    if (liste[i+1]/liste[i]) != raison :
+                        return False
+                else :
+                    return False
+            return True
+        else :
+            return not(any(liste))
+    else :
+        return False
