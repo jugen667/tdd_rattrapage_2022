6fbf0940158e35a23febce97db89b5259dd1aa64
julien genty <julien.genty80@gmail.com>
2023-01-23 01:06:57 +0100

    correction d'erreurs de calcul

--- test_liste_geometrique_remplissage.py
+++ test_liste_geometrique_remplissage.py
@@ -11,10 +11,12 @@ def test_isGeometric_fill():
         assert liste_geometrique_remplissage.isGeometric_fill(2, 3) == False           # if not a list
         print("Test {} passed".format(i))
         i+=1
-        assert liste_geometrique_remplissage.isGeometric_fill([2, 4, 8], 3) == (True,  [2, 4, 8, 16, 32, 64])
+        assert liste_geometrique_remplissage.isGeometric_fill([2, 4, 8], 3) == (True,  [2, 4, 8, 16, 64, 256])
         print("Test {} passed".format(i))
         i+=1
         assert liste_geometrique_remplissage.isGeometric_fill([2, 4, 8], []) == (True,  None)
+        print("Test {} passed".format(i))
+        print("OK")
     except AssertionError :
         print("Test {} not passed".format(i))
         print("KO")
