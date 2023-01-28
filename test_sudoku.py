657e10c7023b58909c59f6558ad2149cb5bcb039
julien genty <julien.genty80@gmail.com>
2023-01-28 22:58:41 +0100

    ajout de tests et corrections d'erreurs pour les tests du sudoku

--- test_sudoku.py
+++ test_sudoku.py
@@ -1,7 +1,7 @@
 import sudoku
 
 def test_verif_sudoku():
-	i = 1
+    i = 1
     try :
         assert sudoku.verif_sudoku([    [1, 2, 3, 7, 8, 9, 4, 5, 6],
                                         [4, 5, 6, 1, 2, 3, 7, 8, 9],
@@ -26,7 +26,49 @@ def test_verif_sudoku():
                                         [9, 7, 8, 6, 4, 5, 3, 1, 1]  ]) == False # wrong grid
         print("Test {} passed".format(i))
         i+=1
-        
+        assert sudoku.verif_sudoku([    [1, 2, 3, 7, 8, 9, 4, 5, 6],
+                                        [4, 5, 6, 1, 2, 3, 7, 8, 9],
+                                        [7, 8, 9, 4, 5, 6, 1, 2, 3],
+                                        [2, 3, 1, 8, 9, 7, 5, 6, 4],
+                                        [5, 6, 4, 2, 3, 1, 8, 9, 7],
+                                        [8, 9, 7, 5, 6, 4, 2, 3, 1],
+                                        [6, 4, 5, 3, 1, 2, 9, 7, 8],
+                                        [9, 7, 8, 6, 4, 5, 3, 1, 2]  ]) == False # wrong size in the grid
+        print("Test {} passed".format(i))
+        i+=1
+        assert sudoku.verif_sudoku([    [],
+                                        [4, 5, 6, 1, 2, 3, 7, 8, 9],
+                                        [7, 8, 9, 4, 5, 6, 1, 2, 3],
+                                        [2, 3, 1, 8, 9, 7, 5, 6, 4],
+                                        [5, 6, 4, 2, 3, 1, 8, 9, 7],
+                                        [8, 9, 7, 5, 6, 4, 2, 3, 1],
+                                        [3, 1, 2, 9, 7, 8, 6, 4, 5],
+                                        [6, 4, 5, 3, 1, 2, 9, 7, 8],
+                                        [9, 7, 8, 6, 4, 5, 3, 1, 2]  ]) == False # empty line in the grid
+        print("Test {} passed".format(i))
+        i+=1
+        assert sudoku.verif_sudoku([    [1, 2, 7, 8, 9, 4, 5, 6],
+                                        [4, 5, 1, 2, 3, 7, 8, 9],
+                                        [7, 8, 4, 5, 6, 1, 2, 3],
+                                        [2, 3, 8, 9, 7, 5, 6, 4],
+                                        [5, 6, 2, 3, 1, 8, 9, 7],
+                                        [8, 9, 5, 6, 4, 2, 3, 1],
+                                        [3, 1, 9, 7, 8, 6, 4, 5],
+                                        [6, 4, 3, 1, 2, 9, 7, 8],
+                                        [9, 7, 6, 4, 5, 3, 1, 2]  ]) == False # wrong line size
+        print("Test {} passed".format(i))
+        i+=1
+        assert sudoku.verif_sudoku([    [1, 2, 3, 7, 8, 9, 4, 5, 6],
+                                        [4, 5, 6, 1, 2, 3, 7, 8, 9],
+                                        [7, 8, 9, 4, 5, 6, 1, 2, 3],
+                                        [2, 3, 1, 8, 9, 7, 5, 6, 4],
+                                        [5, 6, 4, 2, 3, 1, 8, 9, 7],
+                                        [8, 9, 7, 5, 6, 4, 2, 3, 1],
+                                        [3, 1, 2, 9, 7, 8, 'x', 4, 5],
+                                        [6, 4, 5, 3, 1, 2, 9, 7, 8],
+                                        [9, 7, 8, 6, 4, 5, 3, 1, 1]  ]) == False # wrong type in the grid
+        print("Test {} passed".format(i))
+        i+=1
         assert sudoku.verif_sudoku([]) == False          # if the list is empty 
         print("Test {} passed".format(i))
         i+=1
@@ -36,3 +78,5 @@ def test_verif_sudoku():
     except AssertionError :
         print("Test {} not passed".format(i))
         print("KO")
+
+test_verif_sudoku()
